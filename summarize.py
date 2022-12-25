60f59fc7b79aac0e1edf1e18b421b5c079a402b8
theitush <46968791+theitush@users.noreply.github.com>
2022-12-25 16:11:21 +0200

    Update summarize.py
    update mutation data to drop zero frequency (necessary because of previous commit where rounding was implemented)

--- summarize.py
+++ summarize.py
@@ -101,7 +101,7 @@ def graph_summary(freqs_file, blast_file, read_counter_file, stretches_file, out
     plt.subplots_adjust(wspace=0.2, hspace=0.25)
     freqs = pd.read_csv(freqs_file, sep="\t")
     mutation_data = freqs[(freqs['base_rank'] != 0) & (freqs['coverage'] > min_coverage) &
-                          (freqs['base_count'] > 0)].copy()  # TODO: in the old pipeline theres also a probability thing here
+                          (freqs['base_count'] > 0) & (freqs['frequency']>0)].copy()  # TODO: in the old pipeline theres also a probability thing here
     axes[0][0] = graph_blast_length_distribution(blast_file, axes[0][0])
     axes[0][1] = graph_read_counter(read_counter_file, axes[0][1])
     axes[0][2] = graph_coverage(freqs, axes[0][2])
